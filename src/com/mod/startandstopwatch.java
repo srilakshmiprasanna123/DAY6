dc45068f90942f45d2f0e25e6c2a5b0dabba4839
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-03-06 19:25:10 +0530

    initial commit

---
+++ src/com/mod/startandstopwatch.java
@@ -0,0 +1,24 @@
+package com.mod;
+import java.util.Scanner;
+
+public class startandstopwatch {
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.println("press any button to start and end");
+        sc.next();
+        stopwatchSimulator();
+
+    }
+    public static void stopwatchSimulator()
+    {
+        long startTime = System.currentTimeMillis();
+        System.out.println("press any button to start and end");
+        sc.next();
+        long stopTime= System.currentTimeMillis();
+        long time= stopTime - startTime;
+        System.out.println("total time required "+time+" millisecond");
+        System.out.println("total time required "+time/1000+" second");
+
+    }
+}
